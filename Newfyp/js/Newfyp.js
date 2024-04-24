e52a29fbdf56d7f271520cd78304b33e70f46364
TomBallauri <144777722+TomBallauri@users.noreply.github.com>
2024-04-24 16:49:56 +0200

    FYP fini

--- Newfyp/js/Newfyp.js
+++ Newfyp/js/Newfyp.js
@@ -1,11 +1,17 @@
-function toggleComments(elementId) {
-    var commentsContainer = document.getElementById(elementId);
-    var text2 = commentsContainer.parentElement;
-    if (commentsContainer.style.display === "none") {
-        commentsContainer.style.display = "block";
-        text2.classList.add('open-comments'); // Ajoute la classe pour décaler le cadre vers le haut
-    } else {
-        commentsContainer.style.display = "none";
-        text2.classList.remove('open-comments'); // Retire la classe pour ramener le cadre à sa position initiale
-    }
-}
+window.onload = function() {
+    var commentButtons = document.querySelectorAll('.comment-button');
+
+    commentButtons.forEach(function(button) {
+        button.addEventListener('click', function() {
+            var commentsContainer = this.parentElement.nextElementSibling;
+            var text2 = commentsContainer.parentElement;
+            if (commentsContainer.style.display === "none" || commentsContainer.style.display === "") {
+                commentsContainer.style.display = "block";
+                text2.classList.add('open-comments'); // Ajoute la classe pour décaler le cadre vers le haut
+            } else {
+                commentsContainer.style.display = "none";
+                text2.classList.remove('open-comments'); // Retire la classe pour ramener le cadre à sa position initiale
+            }
+        });
+    });
+};
